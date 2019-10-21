25fa503070bc4f8d88b27542032ab6de897892ce
henriquevaz <vaz18004@byui.edu>
2019-10-21 00:05:39 -0600

    project

--- web/week05/registration.php
+++ web/week05/registration.php
@@ -7,8 +7,8 @@ $collect = insert($_POST);
 
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en-us">
 
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -42,15 +42,14 @@ $collect = insert($_POST);
 </div>
 <div id="menu">
 	<ul>
-		<li> <a href="index.php"> |  Home  |</a></li>
+		<li> <a href="home.php"> |  Home  |</a></li>
 		<li>
 		  <a href="login.php">|  Voting  |</a></li>
 		<li>
 		  <a href="result.php">|  Result  |</a></li>
 		<li>
 		  <a href="login.php" >|  Login  |</a></li>
-		<li>
-		  <a href="contact.php">|  Contact Us  |</a></li>
+
 	</ul>
 </div>
 <div id="content">
