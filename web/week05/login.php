6b8ebcdd9a67f54f96ffd07e198bd2ce2cd33a24
henriquevaz <vaz18004@byui.edu>
2019-10-20 23:41:09 -0600

    project

--- web/week05/login.php
+++ web/week05/login.php
@@ -57,7 +57,7 @@ exit;}
 <div id="content">
 	<div id="left">
     <p style="text-align:center; color:#FF0000;"><strong><marquee  behavior="scroll">
-    THIS E-VOTING SYSTEMIS FOR ELEGIBLE NIGERIAN WHO ARE ABOVE 18 YEARS OF AGE AS FROM 23RD JUNE 2012 
+    THIS E-VOTING SYSTEMIS FOR CS313 CLASS 
     </marquee></strong></p>
 </div>
   <th height="41" colspan="2" scope="col"><h1><center>
