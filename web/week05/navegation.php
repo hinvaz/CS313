12f829eadc8b3578b3d6dced873e2bce197fae29
henriquevaz <vaz18004@byui.edu>
2019-10-21 20:18:06 -0600

    project

--- web/week05/navegation.php
+++ web/week05/navegation.php
@@ -1,4 +1,4 @@
-<h1 style="center";>Favorite Restaurant</h1>
+<h1 style="center">Favorite Restaurant</h1>
 
 <ul class="navegation">
   <li class="navegation"><a href="home.php" class="navegation">Restaurants</a></li>
